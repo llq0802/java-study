80f76dc32c46b91322edd4163856d31712e82df8
llq-yyds <958614130@qq.com>
2023-05-14 16:04:47 +0800

    JAVA初始化

---
+++ MySanner.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class MySanner {
+  public static void main(String[] args) {
+    Scanner myScanner = new Scanner(System.in);
+    System.out.println("请输入名字");
+    String name = myScanner.next();
+    System.out.println("请输入年龄");
+    int age = myScanner.nextInt();
+    System.out.println(name + '\t' + age);
+
+  }
+}
---
+++ Var01.java
@@ -0,0 +1,32 @@
+/**
+ * Var01
+ */
+public class Var01 {
+  public static void main(String[] args) {
+    // 整数数字默认为int
+    // 小数点数字默认为double
+
+    char an = 'a'; // 本子也是整数 也就是ASSIC码
+
+    byte n0 = 1; // -128 - 127
+
+    short n1 = 10;
+
+    int n2 = 99;
+
+    float n3 = 1.1F;
+
+    double weight = 122.2;
+
+    String name = "李岚清";
+
+    System.out.println(an);
+    System.out.println(n0);
+    System.out.println(n1);
+    System.out.println(n2);
+    System.out.println(n3);
+    System.out.println(weight);
+    System.out.println(name);
+
+  }
+}
